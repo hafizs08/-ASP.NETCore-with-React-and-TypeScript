4c384392d8ef8c4cc414a1148641297822e265b3
Hafizs Fauzi Ritonga <94980822+hafizs08@users.noreply.github.com>
2024-09-09 08:55:42 +0700

    selesaiLagi

---
+++ rabbit/NETMicroservices/microservices/Data/UserAppServiceContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using microservices.Models;
+
+namespace microservices.Data
+{
+    public class UserAppServiceContext : DbContext
+    {
+        public UserAppServiceContext(DbContextOptions<UserAppServiceContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
